aa619431a7a90943b087100a8be07ddb2b96b650
Sarkar <sneha.b.sarkar@capgemini.com>
2025-02-10 11:12:02 +0530

    Updated Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,19 @@ const Footer = () => {
       <footer className="mb-0 text-center">
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
-            <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href="https://sahibsingh.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Sahib Singh</a>
-            </p>
-            <a className="text-dark fs-4" href="https://github.com/ssahibsingh" target="_blank" rel="noreferrer">
+            <p className="mb-3 mb-md-0 fs-5">Connect with us:</p>
+
+            {/* Social Media Icons */}
+            <a className="text-dark fs-4 mx-3" href="https://instagram.com/YOUR_INSTAGRAM" target="_blank" rel="noreferrer">
+              <i className="fa fa-instagram"></i>
+            </a>
+            <a className="text-dark fs-4 mx-3" href="https://facebook.com/YOUR_FACEBOOK" target="_blank" rel="noreferrer">
+              <i className="fa fa-facebook"></i>
+            </a>
+            <a className="text-dark fs-4 mx-3" href="https://twitter.com/YOUR_TWITTER" target="_blank" rel="noreferrer">
+              <i className="fa fa-twitter"></i>
+            </a>
+            <a className="text-dark fs-4 mx-3" href="https://github.com/YOUR_GITHUB" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
             </a>
           </div>
